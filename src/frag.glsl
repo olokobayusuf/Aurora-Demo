994a34c072fa8ac890bcfd69ee2ad374930db3cd
Yusuf Olokoba <olokobayusuf@gmail.com>
2017-11-15 20:05:21 -0500

    More stubs

--- src/frag.glsl
+++ src/frag.glsl
@@ -40,6 +40,7 @@ const Sphere scene[2] = Sphere[] (
 
 #pragma region --Top level--
 
+Ray generateRay (const vec2 uv, const Camera camera);
 vec3 radiance (const Ray ray);
 varying vec2 uv;
 
@@ -48,11 +49,11 @@ varying vec2 uv;
 * We generate a way for the `uv` position and accumulate its radiance
 */
 void main () {
+    // Create a camera
+    Camera camera = Camera(mat4(1.0), 2.0);
+    camera.transform[3] = vec4(0.0, 0.0, -5.0, 1.0);
     // Create a ray from the `uv` coordinates
-    vec3 position = vec3(0.0, 0.0, -5.0);
-    vec3 planePoint = vec3(uv.x - 0.5, uv.y - 0.5, -2.0);
-    vec3 direction = planePoint - position;
-    Ray ray = Ray(position, direction, vec2(1e-5, 1e+5), 0, vec3(0.0));
+    Ray ray = generateRay(uv, camera);
     // Set the color
     gl_FragColor = vec4(radiance(ray), 1.0);
 }
@@ -82,5 +83,12 @@ vec3 shade (const Ray ray) { // INCOMPLETE
 */
 bool intersect (inout Ray ray, const Sphere sphere) { // INCOMPLETE
     return false;
+}
+
+Ray generateRay (const vec2 uv, const Camera camera) { // INCOMPLETE
+    vec3 position = camera.transform[3].xyz;
+    vec3 planePoint = vec3(uv.x - 0.5, uv.y - 0.5, -camera.fov);
+    vec3 direction = planePoint - position;
+    return Ray(position, direction, vec2(1e-5, 1e+5), 0, vec3(0.0));
 }
 #pragma endregion
